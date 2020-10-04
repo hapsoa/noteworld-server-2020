2713a7f60b1f31c633fbc7e3be0343ec03dcf0fe
Jaejong Heo <39189062+hapsoa@users.noreply.github.com>
2020-10-04 15:29:33 +0900

    update mongodb

--- index.ts
+++ index.ts
@@ -1,6 +1,29 @@
-// const express = require("express");
-
 import express from "express";
+
+import { MongoClient } from "mongodb";
+
+// MongoDB code
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("sample_mflix");
+    const collection = database.collection("movies");
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: "Back to the Future" };
+    const movie = await collection.findOne(query);
+
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 const app = express();
 const port = 3000;
